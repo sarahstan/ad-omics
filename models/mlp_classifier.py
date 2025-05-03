b5bd509baebd4d848e20adc6f9350ef0ef81004c
JJ Stankowicz <jj.stankowicz@gmail.com>
2025-05-03 00:48:44 -0400

    Add MLP classifier.

--- models/mlp_classifier.py
+++ models/mlp_classifier.py
@@ -22,26 +22,3 @@ class ADClassifier(torch.nn.Module):
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.model(x)
-
-
-# class ADClassifier(torch.nn.Module):
-#     def __init__(self, input_dim: int, hidden_dims: list[int]):
-#         super(ADClassifier, self).__init__()
-#         self.input_dim = input_dim
-#         self.hidden_dims = hidden_dims
-
-#         # Create a list of layers
-#         layers = []
-#         in_dim = input_dim
-#         for dim in hidden_dims:
-#             layers.append(torch.nn.Linear(in_dim, dim))
-#             layers.append(torch.nn.ReLU())
-#             in_dim = dim
-
-#         # Add the final output layer
-#         layers.append(torch.nn.Linear(in_dim, 1))
-#         layers.append(torch.nn.Sigmoid())
-
-#         # Combine all layers into a sequential model
-#         self.model = torch.nn.Sequential(*layers)
-#         self.loss_fn = torch.nn.BCELoss()
