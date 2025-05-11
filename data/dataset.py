d261fdf5fe3ac5544704e84b84650c1d8a2f5942
JJ Stankowicz <jj.stankowicz@gmail.com>
2025-05-11 16:14:59 -0400

    Start dataset and its test.

--- data/dataset.py
+++ data/dataset.py
@@ -5,14 +5,42 @@ from torch.utils.data import Dataset
 
 
 class ADOmicsDataset(Dataset):
-    def __init__(self, data_path: str):
-        self.data_path = data_path
-        self.scdata = scDATA(data_path)
+    def __init__(self, scDATA: scDATA, subset: str = "train") -> None:
+        """_summary_
+
+        Args:
+            data_path (str): The path to the data directory.
+            subset (str, optional): The subset of the data to load. Defaults to "train".
+
+        Raises:
+            ValueError: If the subset is not recognized.
+        """
+        self.scdata = scDATA
+        self.subset = subset
+        self.load_subset()
+
+    def load_subset(self) -> None:
+        # Ensure scdata has already done the split
+        attrs = [
+            "train_adata",
+            "test_adata",
+            "val_adata",
+            "train_metadata",
+            "test_metadata",
+            "val_metadata",
+        ]
+        for attr in attrs:
+            if not hasattr(self.scdata, attr):
+                error_str = f"scDATA object does not have attribute {attr}. "
+                error_str += "Split with scdata.split_patient_level()"
+                raise ValueError(error_str)
+        self.adata = getattr(self.scdata, f"{self.subset}_adata")
+        self.metadata = getattr(self.scdata, f"{self.subset}_metadata")
 
     def __len__(self) -> int:
-        return self.counts.shape[0]
+        return self.adata.shape[0]
 
     def __getitem__(self, index: int) -> Tuple[torch.Tensor, torch.Tensor]:
-        data = torch.from_numpy(self.counts.X.toarray()[index])
-        label = torch.tensor(self.labels.iloc[index]).to(torch.float32)
+        data = torch.from_numpy(self.adata.X.toarray()[index])
+        label = torch.tensor(self.metadata.iloc[index]).to(torch.float32)
         return data, label
