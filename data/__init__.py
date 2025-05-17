5abc264ec52ca0357123cb626b59e74beb2fbfbc
JJ Stankowicz <jj.stankowicz@gmail.com>
2025-05-17 00:31:52 -0400

    Add dataset types.

--- data/__init__.py
+++ data/__init__.py
@@ -1,4 +1,5 @@
 from .dataset import ADOmicsDataset
 from .scdata import scDATA
+from .types import VectorData, TokenData
 
-__all__ = ["scDATA", "ADOmicsDataset"]
+__all__ = ["scDATA", "ADOmicsDataset", "VectorData", "TokenData"]
