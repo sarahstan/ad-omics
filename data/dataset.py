184b1628c7b9045253e045b57642951854345c18
JJ Stankowicz <jj.stankowicz@gmail.com>
2025-05-17 00:32:16 -0400

    Add tokenized representaion and test.

--- data/dataset.py
+++ data/dataset.py
@@ -1,23 +1,44 @@
 import torch
+from typing import Tuple, Union
 from .scdata import scDATA
-from typing import Tuple
+from .types import VectorData, TokenData
 from torch.utils.data import Dataset
 from sklearn.preprocessing import RobustScaler
+import enum
+
+
+class GeneRepresentation(enum.Enum):
+    VECTOR = "vector"
+    TOKENS = "tokens"
 
 
 class ADOmicsDataset(Dataset):
-    def __init__(self, scDATA: scDATA, subset: str = "train") -> None:
-        """_summary_
+    def __init__(
+        self,
+        scDATA: scDATA,
+        subset: str = "train",
+        representation: str = "vector",
+    ) -> None:
+        """
+        Initialize the ADOmicsDataset.
 
         Args:
             data_path (str): The path to the data directory.
             subset (str, optional): The subset of the data to load. Defaults to "train".
+            representation (str, optional): The gene representation to use. Defaults to "vector".
 
         Raises:
             ValueError: If the subset is not recognized.
         """
         self.scdata = scDATA
         self.subset = subset
+        if representation == "vector":
+            self.gene_representation = GeneRepresentation.VECTOR
+        elif representation == "tokens":
+            self.gene_representation = GeneRepresentation.TOKENS
+        else:
+            error_str = f"Invalid representation: {representation}. " "Choose 'vector' or 'tokens'."
+            raise ValueError(error_str)
         self.load_subset()
 
     def load_subset(self) -> None:
@@ -46,30 +67,66 @@ class ADOmicsDataset(Dataset):
     def __len__(self) -> int:
         return self.adata.shape[0]
 
-    def __getitem__(
-        self,
-        index: int,
-        normalize: bool = True,
-    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-        """
-        Get the data and label for a given index.
-
-        Args:
-            index (int): The index of the data point to retrieve.
-            normalize (bool, optional): Whether to normalize the data. Defaults to True.
-        Returns:
-            Tuple[torch.Tensor, torch.Tensor, torch.Tensor]: The data, cell type, and label.
-        """
-        # Convert the data to a tensor
+    def _get_gene_vector(self, index: int, normalize: bool = True) -> torch.Tensor:
         data = self.adata.X[index].toarray().ravel()  # Extract as 1D array
         if normalize:
             # Normalize the data - reshape to 2D for transform, then back to 1D
             data = data.reshape(1, -1)  # Make it 2D for scaler
             data = self.scaler.transform(data).ravel()  # Transform and flatten back to 1D
         data = torch.from_numpy(data).float()
+        return data
+
+    def _get_gene_tokens(
+        self, index: int, normalize: bool = True
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        data = self.adata.X[index].toarray().ravel()
+        if normalize:
+            # Normalize the data - reshape to 2D for transform, then back to 1D
+            data = data.reshape(1, -1)
+            data = self.scaler.transform(data).ravel()
+        data = torch.from_numpy(data).float()
+        # Return a 2D-tensor of shape [non-zero index, count]
+        non_zero_indices = data.nonzero().ravel()
+        non_zero_values = data[non_zero_indices]
+        return non_zero_indices, non_zero_values
+
+    def __getitem__(
+        self,
+        index: int,
+        normalize: bool = True,
+    ) -> Union[VectorData, TokenData]:
+        """
+        Get the data and label for a given index.
+
+        Args:
+            index (int): The index of the data point to retrieve.
+            normalize (bool, optional): Whether to normalize the data. Defaults to True.
+
+        Returns:
+            Union[VectorData, TokenData]: The data, cell type, and label.
+        """
         # Convert the cell type index to a one-hot encoded vector
         cell_type = torch.zeros(len(self.scdata.cell_types), dtype=torch.float32)
         cell_type[self.metadata.cell_type.iloc[index]] = 1.0
         # Convert the label to a tensor
         label = torch.tensor(self.metadata.label.iloc[index]).to(torch.float32)
-        return data, cell_type, label
+        # Convert the data to a tensor
+        if self.gene_representation == GeneRepresentation.VECTOR:
+            data = self._get_gene_vector(index, normalize)
+            return VectorData(
+                gene_vector=data,
+                cell_type=cell_type,
+                label=label,
+            )
+        elif self.gene_representation == GeneRepresentation.TOKENS:
+            gene_tokens, gene_counts = self._get_gene_tokens(index, normalize)
+            return TokenData(
+                gene_indices=gene_tokens,
+                gene_counts=gene_counts,
+                cell_type=cell_type,
+                label=label,
+            )
+        else:
+            error_str = f"Invalid gene representation: {self.gene_representation}. "
+            error_str += "Choose 'vector' or 'tokens'."
+            raise ValueError(error_str)
