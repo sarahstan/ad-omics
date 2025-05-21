64e48f3a7eb2c6768039f548b87a01d383994d15
JJ Stankowicz <jj.stankowicz@gmail.com>
2025-05-21 15:41:29 -0400

    Add collate functions.

--- data/types.py
+++ data/types.py
@@ -1,6 +1,12 @@
+from typing import Tuple
 from dataclasses import dataclass
 import torch
 from typing import Optional
+
+VectorDataTensorType = Tuple[torch.Tensor, torch.Tensor, torch.Tensor]
+TokenDataTensorType = Tuple[
+    torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, Optional[torch.Tensor]
+]
 
 
 @dataclass
@@ -22,6 +28,17 @@ class VectorData:
     gene_vector: torch.Tensor
     cell_type: torch.Tensor
     label: torch.Tensor
+
+    @staticmethod
+    def collate(batch) -> VectorDataTensorType:
+        """
+        Collate function to handle batching of data.
+        """
+        gene_vector = torch.stack([item.gene_vector for item in batch])
+        cell_type = torch.stack([item.cell_type for item in batch])
+        label = torch.tensor([item.label for item in batch])
+
+        return gene_vector, cell_type, label
 
 
 @dataclass
@@ -49,3 +66,20 @@ class TokenData:
     cell_type: torch.Tensor
     label: torch.Tensor
     attention_mask: Optional[torch.Tensor] = None
+
+    @staticmethod
+    def collate(batch) -> TokenDataTensorType:
+        """
+        Collate function to handle batching of data.
+        """
+        gene_indices = torch.nn.utils.rnn.pad_sequence(
+            [item.gene_indices for item in batch], batch_first=True
+        )
+        gene_counts = torch.nn.utils.rnn.pad_sequence(
+            [item.gene_counts for item in batch], batch_first=True
+        )
+        cell_type = torch.stack([item.cell_type for item in batch])
+        label = torch.tensor([item.label for item in batch])
+        attention_mask = (gene_indices != 0).float()
+
+        return gene_indices, gene_counts, cell_type, label, attention_mask
